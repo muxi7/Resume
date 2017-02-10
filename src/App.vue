b7b217af11a0d2bfe3a5622de4bc78b4458f0abd
muxi7 <1360097606@qq.com>
2017-02-10 23:13:26 +0800

    add resume

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="page">
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
+</template>
+
+<script>
+import 'normalize.css/normalize.css'
+import './assets/reset.css'
+
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
+
+export default {
+  name: 'app',
+  data:function(){
+    return {text:'你好'}
+  },
+  components: {Topbar, ResumeEditor, ResumePreview}
+}
+</script>
+
+<style>
+.page{
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color:#EAEBEC;
+}
+.page>main{
+  flex-grow: 1;  
+}
+
+.page>main{
+  min-width: 1024px;
+  max-width: 1440px;
+  margin-top: 16px;
+  margin-bottom:16px;
+  display: flex;
+  justify-content: space-between;
+  padding:0 16px;
+  align-self:center;
+  width:100%;
+}
+
+#resumeEditor{
+  width: 35%;
+  background: #444;
+}
+#resumePreview{
+  flex-grow:1;
+  margin-left:16px;
+  background: #777;
+}
+</style>
